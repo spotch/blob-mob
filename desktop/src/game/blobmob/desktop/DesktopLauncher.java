4b8c31b1812c78fe3929d240445ae1481a23cf49
Adam Schneider <spotchious@gmail.com>
2014-08-22 22:14:01 -0500

    - adding a working base project

---
+++ desktop/src/game/blobmob/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package game.blobmob.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.blobmob.BlobMob;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BlobMob(), config);
+	}
+}
